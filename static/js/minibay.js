3baa5d0023fe91152e455a7f2e74672fc05ff1e9
Dionysis Zindros <dionyziz@gmail.com>
2013-03-25 15:37:39 +0200

    Visual changes

--- static/js/minibay.js
+++ static/js/minibay.js
@@ -1,7 +1,13 @@
-if ($('#minibay-search')) {
+if ($('#minibay-search').length) {
     $('input')[0].focus();
+    $('input')[0].select();
 }
-if ($('form.search')) {
+if ($('#minibay-listen').length) {
+    $('input')[0].focus();
+    $('input')[0].select();
+}
+
+if ($('form.search').length) {
     $('form.search')[0].onsubmit = function() {
         window.location.href = '/bay/search/' + document.getElementById('q').value;
         return false;
